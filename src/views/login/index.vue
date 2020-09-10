524b59f56e4c543b4814892bc6f3927c9daf603d
wangxy1997 <849067918@qq.com>
2020-09-10 20:01:24 +0800

    登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,21 @@
     <!--雪花容器-->
     <div id="page_end_html"></div>
     <div class="login">
-11111
+      <p class="login_title">欢迎登录后台管理系统</p>
+      <el-form class="login_form" ref="form" :model="form">
+        <el-form-item>
+          <i :class="isName ? 'el-icon-user': 'icon_opacity el-icon-user'"></i>
+          <el-input @focus="isName = true" @blur="isName = false" class="login_input"
+                    v-model="form.name" placeholder="用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <i :class="ispassword ? 'el-icon-lock': 'icon_opacity el-icon-lock'"></i>
+          <el-input @focus="ispassword = true" @blur="ispassword = false" class="login_input"
+                    v-model="form.password" placeholder="密码"></el-input>
+        </el-form-item>
+
+          <button><span>登 录</span></button>
+      </el-form>
     </div>
   </div>
 </template>
@@ -17,6 +31,12 @@ export default {
   components: { editInfo },
   data () {
     return {
+      isName: false,
+      ispassword: false,
+      form: {
+        name: '',
+        password: ''
+      }
     }
   },
   mounted () {
